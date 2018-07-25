afe7333b8acc74baa134d024b73e14e8ce14c28d
yuhanyuhan <yuhanlian@gmail.com>
2018-07-25 14:31:15 +0800

    pushing!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    this.getData("");
+  }
+
+  getData = async search => {
+    const getBooks = await fetch(
+      `http://localhost:3000/books`
     );
+    const bookList  = await getBooks.json();
+    this.setState({
+      books: bookList
+    })
+  };
+
+  render() {
+    return <div>
+    {this.state.books.map((book,id) => 
+      <li key={book._id}> 
+      {book.title} 
+      </li>)}</div>;
   }
 }
 
