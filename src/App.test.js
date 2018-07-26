2ec3ddb8322811c43712673ccb96ed05d477e7cb
yuhanyuhan <yuhanlian@gmail.com>
2018-07-26 14:35:55 +0800

    added shallow rendering and snapshot testing, super cool!

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SomeComponent from './SomeComponent';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
-it.skip('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders correctly', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<SomeComponent />);
+  const result = renderer.getRenderOutput();
+  expect(result).toMatchSnapshot()
 });
+
+
+
+// it.skip('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
---
+++ src/SomeComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'; 
+
+const SomeComponent = () => {
+    return <p> Hello  </p>
+}
+
+export default SomeComponent; 
